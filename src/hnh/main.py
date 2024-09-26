23554c07b9a626db19781a3e93edff7dbbe90458
NishNovae <raecrowned@gmail.com>
2024-09-26 09:49:47 +0900

    dummy

--- src/hnh/main.py
+++ src/hnh/main.py
@@ -1,1 +1,16 @@
 # src/hnh/main.py
+
+import random
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+@app.post("/")
+def read_root():
+    return { "Hello": "World!" }
+
+@app.post("/hotdog")
+def is_hotdog():
+    # dummy!
+    hotdog = random.choice(["True", "False"])
+    return { "dummy": "code", "hotdog": hotdog }
